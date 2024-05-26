da5a566e4db22f9a00b5d7d2016c40f1bdbfae9d
nemanjaraduljica <nemanja.raduljica@student.um.si>
2024-05-26 20:24:49 +0200

    First commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/neman/OneDrive/Documents/FERI/RG/PRO_test
+# Build directory: C:/Users/neman/OneDrive/Documents/FERI/RG/PRO_test/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
